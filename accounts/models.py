1b70651527f062285df728893004b623bf80390f
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-10-14 14:41:20 +0500

    Added email field as default username field with custom error messages

--- accounts/models.py
+++ accounts/models.py
@@ -6,5 +6,15 @@ from django.contrib.auth.models import (
 
 # Create your models here.
 class User(AbstractBaseUser):
-    pass
+    email       = models.EmailField(
+        _('Email'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Valid email addresses only.'),
+        error_messages={
+            'unique': _("That email address is already registered."),
+        },
+    )
+
+    USERNAME_FIELD = 'email'
     
