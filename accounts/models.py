66788a5d7a7cc169c5946c5f50f18c3cbb15b421
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-10-14 14:40:23 +0500

    Added my Own User class

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import (
+    AbstractBaseUser
+)
 
 # Create your models here.
+class User(AbstractBaseUser):
+    pass
+    
