3cb27bd627e679cf6e14b78f5dc3a60d5b17ba35
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-10-14 14:55:31 +0500

    Added UserManager class

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import (
-    AbstractBaseUser
+    AbstractBaseUser,
+    BaseUserManager
 )
+
+
+class UserManager(BaseUserManager):
+    pass
+
 
 # Create your models here.
 class User(AbstractBaseUser):
@@ -32,6 +38,8 @@ class User(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
