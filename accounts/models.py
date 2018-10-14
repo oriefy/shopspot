ccf54322f577bc9c2a04bf1f580879c2932e7490
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-10-14 15:28:32 +0500

    fixed a bug

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import (
     AbstractBaseUser,
