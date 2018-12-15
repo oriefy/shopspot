78d8f6d0dcea5a623b354a6bde8fa0c1c98a7d94
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-12-16 03:42:49 +0500

    add new urls

--- shopspot/urls.py
+++ shopspot/urls.py
@@ -21,11 +21,12 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('registration.backends.default.urls')),
+    path('', include('explorer.urls')),
 
-    path('accounts/profile/',
-        TemplateView.as_view(template_name='profile.html'),
-        name='profile'),
+    path('admin/', admin.site.urls),
+    # path('accounts/', include('registration.backends.default.urls')),
+
+    # path('accounts/profile/',
+    #     TemplateView.as_view(template_name='profile.html'),
+    #     name='profile'),
 ]
