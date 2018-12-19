680e864ee5af36d0410925284ba1fc5a5de22062
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-12-20 01:43:11 +0500

    include account urls in main urlconf

--- shopspot/urls.py
+++ shopspot/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', include('explorer.urls')),
-
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     # path('accounts/', include('registration.backends.default.urls')),
 
