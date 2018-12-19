1d3566084d50387b27646205d17b00283ece1dc0
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-12-20 01:43:46 +0500

    create account urls

---
+++ accounts/urls.py
@@ -0,0 +1,26 @@
+"""shopspot URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+
+from django.views.generic import TemplateView
+from accounts.views import BuyerSignUpView, SellerSignUpView
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('signup/', TemplateView.as_view(template_name='registration/signup.html'), name='signup'),
+    path('signup/buyer/', BuyerSignUpView.as_view(), name='buyer_signup'),
+    path('signup/seller/', SellerSignUpView.as_view(), name='seller_signup'),
+]
